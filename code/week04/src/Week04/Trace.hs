283b8ea094cf3a6d75f366977cc9f98388953854
kindofdev <jose.velasco.home@gmail.com>
2021-07-25 14:08:53 +0200

    add trace emulator test

--- code/week04/src/Week04/Trace.hs
+++ code/week04/src/Week04/Trace.hs
@@ -32,3 +32,38 @@ myTrace = do
     callEndpoint @"grab" h2 ()
     s <- waitNSlots 1
     Extras.logInfo $ "reached " ++ show s
+
+happypathTrace :: EmulatorTrace ()
+happypathTrace = do
+    let (w1, w2, w3) = (Wallet 1, Wallet 2, Wallet 3)
+    h1 <- activateContractWallet w1 endpoints
+    h2 <- activateContractWallet w2 endpoints
+    h3 <- activateContractWallet w3 endpoints
+    callEndpoint @"give" h1 $ GiveParams 
+        { gpBeneficiary = pubKeyHash $ walletPubKey w2
+        , gpDeadline    = slotToBeginPOSIXTime def 10
+        , gpAmount      = 10000000
+        }
+    void $ Emulator.waitNSlots 1    
+
+    callEndpoint @"give" h1 $ GiveParams 
+        { gpBeneficiary = pubKeyHash $ walletPubKey w2
+        , gpDeadline    = slotToBeginPOSIXTime def 20
+        , gpAmount      = 10000000
+        }    
+    void $ Emulator.waitNSlots 1 
+    
+    callEndpoint @"give" h1 $ GiveParams 
+        { gpBeneficiary = pubKeyHash $ walletPubKey w3
+        , gpDeadline    = slotToBeginPOSIXTime def 10
+        , gpAmount      = 10000000
+        }    
+    void $ Emulator.waitUntilSlot 10
+    callEndpoint @"grab" h3 ()   
+    void $ Emulator.waitUntilSlot 20
+    callEndpoint @"grab" h2 ()   
+    void $ Emulator.waitNSlots 1
+    Extras.logInfo @String "DONE"
+
+testHappypath :: IO ()
+testHappypath = runEmulatorTraceIO happypathTrace
