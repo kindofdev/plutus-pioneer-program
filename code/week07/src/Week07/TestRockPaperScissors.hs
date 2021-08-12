a033614ff2fab0787fd1f0d17054dba16d19edc8
kindofdev <jose.velasco.home@gmail.com>
2021-08-12 20:17:43 +0200

    fix rebasing

--- code/week07/src/Week07/TestRockPaperScissors.hs
+++ code/week07/src/Week07/TestRockPaperScissors.hs
@@ -14,32 +14,36 @@
 
 module Week07.TestRockPaperScissors where
 
-import Control.Monad              hiding (fmap)
-import Control.Monad.Freer.Extras as Extras
-import Data.Default               (Default (..))
-import Ledger
-import Ledger.TimeSlot
-import Plutus.Trace.Emulator      as Emulator
-import PlutusTx.Prelude
-import Prelude                    (IO, Show (..))
-import Wallet.Emulator.Wallet
+import           Control.Monad              hiding (fmap)
+import           Control.Monad.Freer.Extras as Extras
+import           Data.Default               (Default (..))
+import           Ledger
+import           Ledger.TimeSlot
+import           Plutus.Trace.Emulator      as Emulator
+import           PlutusTx.Prelude
+import           Prelude                    (IO, Show (..))
+import           Wallet.Emulator.Wallet
 
-import Week07.RockPaperScissors
+import           Week07.RockPaperScissors
 
 test :: IO ()
 test = do
-    test' Rock     Rock
-    test' Rock     Paper
-    test' Rock     Scissors
-    test' Paper    Rock
-    test' Paper    Paper
-    test' Paper    Scissors
-    test' Scissors Rock
-    test' Scissors Paper
-    test' Scissors Scissors
+    test'  Rock     Rock
+    test'  Rock     Paper
+    test'  Rock     Scissors
+    test'  Scissors Rock
+    test'  Scissors Paper
+    test'  Scissors Scissors
+    test'  Paper    Rock
+    test'  Paper    Paper
+    test'  Paper    Scissors
+    test'' Rock 
 
 test' :: GameChoice -> GameChoice -> IO ()
 test' c1 c2 = runEmulatorTraceIO $ myTrace c1 c2
+
+test'' :: GameChoice -> IO ()
+test'' = runEmulatorTraceIO . myTrace'
 
 myTrace :: GameChoice -> GameChoice -> EmulatorTrace ()
 myTrace c1 c2 = do
@@ -64,7 +68,9 @@ myTrace c1 c2 = do
                 }
 
     callEndpoint @"first" h1 fp
+
     tt <- getTT h1
+
     let sp = SecondParams
                 { spFirst          = pkh1
                 , spStake          = stake
@@ -73,6 +79,8 @@ myTrace c1 c2 = do
                 , spChoice         = c2
                 , spToken          = tt
                 }
+
+    void $ Emulator.waitNSlots 3
 
     callEndpoint @"second" h2 sp
 
@@ -85,3 +93,28 @@ myTrace c1 c2 = do
         case m of
             Nothing -> getTT h
             Just tt -> Extras.logInfo ("read thread token " ++ show tt) >> return tt
+
+
+myTrace' :: GameChoice -> EmulatorTrace ()
+myTrace' c1 = do
+    Extras.logInfo $ "first and unique move: " ++ show c1 
+
+    h1 <- activateContractWallet (Wallet 1) endpoints
+
+    let pkh2      = pubKeyHash $ walletPubKey $ Wallet 2
+        stake     = 5_000_000
+        deadline1 = slotToEndPOSIXTime def 5
+        deadline2 = slotToEndPOSIXTime def 10
+
+        fp = FirstParams
+                { fpSecond         = pkh2
+                , fpStake          = stake
+                , fpPlayDeadline   = deadline1
+                , fpRevealDeadline = deadline2
+                , fpNonce          = "SECRETNONCE"
+                , fpChoice         = c1
+                }
+
+    callEndpoint @"first" h1 fp
+    void $ Emulator.waitNSlots 10
+
