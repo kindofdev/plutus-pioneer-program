261e554e1f4723c17ffae624c4414c23bb8cdc31
kindofdev <jose.velasco.home@gmail.com>
2021-08-16 18:25:43 +0200

    homework week 8

--- code/week08/test/Spec.hs
+++ code/week08/test/Spec.hs
@@ -2,15 +2,16 @@ module Main
     ( main
     ) where
 
-import qualified Spec.Model
-import qualified Spec.Trace
+import qualified Spec.ModelClosable
+import qualified Spec.TraceClosable
 import           Test.Tasty
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "token sale"
-    [ Spec.Trace.tests
-    , Spec.Model.tests
+tests = testGroup "token sale closable"
+    [ Spec.TraceClosable.tests
+    , Spec.TraceClosable.tests2
+    , Spec.ModelClosable.tests
     ]
