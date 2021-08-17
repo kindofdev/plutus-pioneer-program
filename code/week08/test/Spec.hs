579d4fe27c8ca244b49edfde9ab87a7f7b9cfba5
kindofdev <jose.velasco.home@gmail.com>
2021-08-18 01:19:48 +0200

    merge conflict

--- code/week08/test/Spec.hs
+++ code/week08/test/Spec.hs
@@ -2,19 +2,32 @@ module Main
     ( main
     ) where
 
+<<<<<<< HEAD
 import qualified Spec.Model
 import qualified Spec.ModelWithClose
 import qualified Spec.Trace
 import qualified Spec.TraceWithClose
+=======
+import qualified Spec.ModelClosable
+import qualified Spec.TraceClosable
+>>>>>>> homework week 8
 import           Test.Tasty
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
+<<<<<<< HEAD
 tests = testGroup "token sale"
     [ Spec.Trace.tests
     , Spec.TraceWithClose.tests
     , Spec.Model.tests
     , Spec.ModelWithClose.tests
     ]
+=======
+tests = testGroup "token sale closable"
+    [ Spec.TraceClosable.tests
+    , Spec.TraceClosable.tests2
+    , Spec.ModelClosable.tests
+    ]
+>>>>>>> homework week 8
